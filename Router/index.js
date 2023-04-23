352cd28b8aa6f8e5a5833c11f698741c85e6f8b6
shaikh aamir <101973622+shaikhaamir2508@users.noreply.github.com>
2023-04-23 11:43:30 +0530

    add task

--- Router/index.js
+++ Router/index.js
@@ -5,16 +5,16 @@ const Task = require("../Models/task");
 router.get("/addTask", async (req, res) => {
   try {
     console.log(req.body);
-    // const { primarytext, description, headline, cta, imageurl } = req.body;
+    const { primarytext, description, headline, cta, imageurl } = req.body;
 
-    // const saveTask = await Task({
-    //   primarytext,
-    //   description,
-    //   headline,
-    //   cta,
-    //   imageurl,
-    // });
-    // await saveTask();
+    const saveTask = await Task({
+      primarytext,
+      description,
+      headline,
+      cta,
+      imageurl,
+    });
+    await saveTask();
     res.status(200).send("task Added");
   } catch (error) {
     console.log(error);
