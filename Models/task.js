37c2e6e6906ed4b9989ba3480c851ea3d4f46ad7
satyam-bidve <satyambidve@gmail.com>
2023-04-23 11:39:27 +0530

    second with data add

--- Models/task.js
+++ Models/task.js
@@ -1,6 +1,9 @@
 const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema({
+  companyId : {
+    type: Number,
+  },
   primarytext: {
     type: String,
   },
--- Router/index.js
+++ Router/index.js
@@ -2,19 +2,20 @@ const express = require("express");
 const router = express.Router();
 const Task = require("../Models/task");
 
-router.get("/addTask", async (req, res) => {
+router.post("/addTask", async (req, res) => {
   try {
-    console.log(req.body);
-    // const { primarytext, description, headline, cta, imageurl } = req.body;
+    //console.log(req.body)
+    const {companyId , primarytext, description, headline, cta, imageurl } = req.body;
 
-    // const saveTask = await Task({
-    //   primarytext,
-    //   description,
-    //   headline,
-    //   cta,
-    //   imageurl,
-    // });
-    // await saveTask();
+    const saveTask = await Task({
+      companyId ,
+      primarytext,
+      description,
+      headline,
+      cta,
+      imageurl,
+    });
+    await saveTask.save();
     res.status(200).send("task Added");
   } catch (error) {
     console.log(error);
